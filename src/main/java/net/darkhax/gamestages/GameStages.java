43ddfd490c9d42cc274f5b978aa8517c11854b1c
Tyler Hancock <darklime@live.ca>
2017-07-15 15:24:52 -0600

    Ported to 1.12.

--- src/main/java/net/darkhax/gamestages/GameStages.java
+++ src/main/java/net/darkhax/gamestages/GameStages.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = "gamestages", name = "Game Stages", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.0.0.387,);")
+@Mod(modid = "gamestages", name = "Game Stages", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.0.0.414,);")
 public class GameStages {
 
     public static final NetworkHandler NETWORK = new NetworkHandler("gamestages");
