df267d152b8b04a430764bb69cea1e62bff6b6d0
Tyler Hancock <darklime@live.ca>
2017-08-23 18:29:02 -0600

    Updated formatting of GameStageEvent docs.

--- src/main/java/net/darkhax/gamestages/event/GameStageEvent.java
+++ src/main/java/net/darkhax/gamestages/event/GameStageEvent.java
@@ -9,59 +9,59 @@ import net.minecraftforge.fml.common.eventhandler.Event;
  * treated as an event.
  */
 public class GameStageEvent extends Event {
-    
+
     /**
      * The player the event is for.
      */
     private final EntityPlayer player;
-    
+
     /**
      * The stage the event is for. This stage can be changed by other mods listening to the event!
      */
     private String stageName;
-    
+
     /**
      * The base constructor for all game stage events.
-     * 
+     *
      * @param player The player the event is for.
      * @param stageName The stage the event is for.
      */
     public GameStageEvent (EntityPlayer player, String stageName) {
-        
+
         this.player = player;
         this.stageName = stageName;
     }
-    
+
     /**
      * Gets the player the event is for.
-     * 
+     *
      * @return The player that the event was fired for.
      */
     public EntityPlayer getPlayer () {
-        
+
         return this.player;
     }
-    
+
     /**
      * Gets the stage name for the event. Note that other listeners can change this!
-     * 
+     *
      * @return The stage name for the event.
      */
     public String getStageName () {
-        
+
         return this.stageName;
     }
-    
+
     /**
      * Sets the stage for the event. This can be used to change or manipulate the stage name.
-     * 
+     *
      * @param stage The new name for the stage.
      */
     public void setStage (String stage) {
-        
+
         this.stageName = stage;
     }
-    
+
     /**
      * This event is fired every time a stage is added to a player, or unlocked. This event is
      * fired on both the client and the server. This event is still fired, even if the player has
@@ -69,13 +69,13 @@ public class GameStageEvent extends Event {
      */
     @Cancelable
     public static class Add extends GameStageEvent {
-        
+
         public Add (EntityPlayer player, String stageName) {
-            
+
             super(player, stageName);
         }
     }
-    
+
     /**
      * This event is fired every time a stage is removed from a player, or locked. This event is
      * fired on both the client and the server. This event is still fired, even if the player does
@@ -84,17 +84,17 @@ public class GameStageEvent extends Event {
      */
     @Cancelable
     public static class Remove extends GameStageEvent {
-        
+
         public Remove (EntityPlayer player, String stageName) {
-            
+
             super(player, stageName);
         }
     }
-    
+
     /**
      * This event is fired every time a check for a stage is made. This event can be canceled,
      * which will make the check fail.
-     * 
+     *
      * Note: To ensure this event is reliable, all mods which add support for GameStages should
      * make use of
      * {@link net.darkhax.gamestages.capabilities.PlayerDataHandler.IStageData#hasUnlockedStage(String)}.
@@ -106,9 +106,9 @@ public class GameStageEvent extends Event {
      */
     @Cancelable
     public static class Check extends GameStageEvent {
-        
+
         public Check (EntityPlayer player, String stageName) {
-            
+
             super(player, stageName);
         }
     }
