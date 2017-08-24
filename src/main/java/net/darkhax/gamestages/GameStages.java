5123de979139fcdf362efb969d8e5bcdb22e2aa1
Tyler Hancock <darklime@live.ca>
2017-08-23 22:33:04 -0600

    Updated bookshelf, and added new bookshelf code.

--- src/main/java/net/darkhax/gamestages/GameStages.java
+++ src/main/java/net/darkhax/gamestages/GameStages.java
@@ -1,7 +1,9 @@
 package net.darkhax.gamestages;
 
+import net.darkhax.bookshelf.BookshelfRegistry;
 import net.darkhax.bookshelf.command.CommandTree;
 import net.darkhax.bookshelf.network.NetworkHandler;
+import net.darkhax.bookshelf.world.gamerule.GameRule;
 import net.darkhax.gamestages.capabilities.PlayerDataHandler;
 import net.darkhax.gamestages.capabilities.PlayerDataHandler.DefaultStageData;
 import net.darkhax.gamestages.capabilities.PlayerDataHandler.IStageData;
@@ -15,28 +17,26 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = "gamestages", name = "Game Stages", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.0.0.414,);")
+@Mod(modid = "gamestages", name = "Game Stages", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.0.0.425,);")
 public class GameStages {
 
     public static final NetworkHandler NETWORK = new NetworkHandler("gamestages");
     public static final CommandTree COMMAND = new CommandStageTree();
+    public static final GameRule GAME_RULE_SHARE_STAGES = new GameRule("shareGameStages", false);
 
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
 
+        // Packets
         NETWORK.register(PacketStage.class, Side.CLIENT);
         NETWORK.register(PacketStageAll.class, Side.CLIENT);
         NETWORK.register(PacketRequestClientSync.class, Side.SERVER);
+
         CapabilityManager.INSTANCE.register(IStageData.class, new Storage(), DefaultStageData.class);
         MinecraftForge.EVENT_BUS.register(new PlayerDataHandler());
-    }
-
-    @EventHandler
-    public void serverStarting (FMLServerStartingEvent event) {
-
-        event.registerServerCommand(COMMAND);
+        BookshelfRegistry.addCommand(COMMAND);
+        BookshelfRegistry.addGameRule(GAME_RULE_SHARE_STAGES);
     }
 }
