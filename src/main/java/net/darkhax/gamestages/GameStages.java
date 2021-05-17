6dde7b0989a19a47b1753330988949b4a078265e
Tyler Hancock <darklime@live.ca>
2021-05-17 16:24:34 -0600

    Add debug info to the F3 screen.

--- src/main/java/net/darkhax/gamestages/GameStages.java
+++ src/main/java/net/darkhax/gamestages/GameStages.java
@@ -9,13 +9,19 @@ import net.darkhax.bookshelf.util.ModUtils;
 import net.darkhax.gamestages.addons.crt.CraftTweakerEventSubscription;
 import net.darkhax.gamestages.command.GameStageCommands;
 import net.darkhax.gamestages.data.GameStageSaveHandler;
+import net.darkhax.gamestages.data.IStageData;
 import net.darkhax.gamestages.packet.MessageStages;
 import net.darkhax.gamestages.packet.NetworkHandlerClient;
 import net.darkhax.gamestages.packet.NetworkHandlerServer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(GameStages.MOD_ID)
 public class GameStages {
@@ -32,14 +38,42 @@ public class GameStages {
         GameStageSaveHandler.reloadKnownStages();
         GameStageCommands.initializeCommands(REGISTRY);
         REGISTRY.initialize(FMLJavaModLoadingContext.get().getModEventBus());
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-    }
-    
-    private void setup (FMLCommonSetupEvent event) {
+        
+        if (FMLEnvironment.dist.isClient()) {
+            
+            MinecraftForge.EVENT_BUS.addListener(this::onF3Text);
+        }
         
         if (ModUtils.isInModList("crafttweaker")) {
             
             MinecraftForge.EVENT_BUS.register(CraftTweakerEventSubscription.class);
+        }
+    }
+    
+    @OnlyIn(Dist.CLIENT)
+    private void onF3Text (RenderGameOverlayEvent.Text event) {
+        
+        final Minecraft mc = Minecraft.getInstance();
+        
+        if (mc.options.renderDebug) {
+            
+            if (mc.player.isShiftKeyDown()) {
+                
+                final IStageData data = GameStageHelper.getPlayerData(mc.player);
+                
+                if (data != null) {
+                    
+                    event.getRight().add(TextFormatting.GOLD + TextFormatting.UNDERLINE.toString() + "GameStages");
+                    event.getRight().add("Count: " + data.getStages().size());
+                    event.getRight().add("Type: " + data.getClass().getName());
+                    event.getRight().add("Stages: " + data.getStages().toString());
+                }
+            }
+            
+            else {
+                
+                event.getRight().add(TextFormatting.GOLD + TextFormatting.UNDERLINE.toString() + "GameStages [Shift]");
+            }
         }
     }
 }
