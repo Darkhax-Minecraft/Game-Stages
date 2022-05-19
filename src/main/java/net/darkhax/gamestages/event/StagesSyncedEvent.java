7da889686c9d528c9e879ae860a838bc666951f1
Tyler Hancock <darklime@live.ca>
2022-05-19 14:45:09 -0600

    Fix server packet class being removed on the server. #72

--- src/main/java/net/darkhax/gamestages/event/StagesSyncedEvent.java
+++ src/main/java/net/darkhax/gamestages/event/StagesSyncedEvent.java
@@ -3,11 +3,8 @@ package net.darkhax.gamestages.event;
 import net.darkhax.gamestages.data.IStageData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
-@OnlyIn(Dist.CLIENT)
 public class StagesSyncedEvent extends PlayerEvent {
     
     private final IStageData data;
--- src/main/java/net/darkhax/gamestages/packet/GameStagesPacketHandler.java
+++ src/main/java/net/darkhax/gamestages/packet/GameStagesPacketHandler.java
@@ -9,7 +9,6 @@ import net.darkhax.gamestages.event.StagesSyncedEvent;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.NetworkRegistry;
