5e602b318e0381d23ba3995fe3a862b605d54cbd
Tyler Hancock <darklime@live.ca>
2023-02-14 13:51:21 -0700

    Added support for CraftTweaker again.

---
+++ src/main/java/net/darkhax/gamestages/addons/crt/natives/event/ZenGameStageEvent.java
@@ -0,0 +1,41 @@
+package net.darkhax.gamestages.addons.crt.natives.event;
+
+import javax.annotation.Nonnull;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import org.openzen.zencode.java.ZenCodeType;
+
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+
+import net.darkhax.gamestages.event.GameStageEvent;
+
+@ZenRegister
+@NativeTypeRegistration(value = GameStageEvent.class, zenCodeName = "mods.gamestages.events.GameStageEvent")
+public class ZenGameStageEvent {
+    
+    @Nonnull
+    @ZenCodeType.Method
+    @ZenCodeType.Getter("stage")
+    public static String getStageName (GameStageEvent event) {
+        
+        return event.getStageName();
+    }
+    
+    @ZenRegister
+    @NativeTypeRegistration(value = GameStageEvent.Added.class, zenCodeName = "mods.gamestages.events.GameStageAdded")
+    public static class Added {
+        
+    }
+    
+    @ZenRegister
+    @NativeTypeRegistration(value = GameStageEvent.Removed.class, zenCodeName = "mods.gamestages.events.GameStageRemoved")
+    public static class Removed {
+        
+    }
+    
+    @ZenRegister
+    @NativeTypeRegistration(value = GameStageEvent.Cleared.class, zenCodeName = "mods.gamestages.events.GameStageCleared")
+    public static class Cleared {
+        
+    }
+}
